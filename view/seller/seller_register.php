a20fec7eccbaeb9ced4198364d6cd23340d35697
loljaar <vaghelajatin5@example.com>
2024-03-02 09:05:04 +0530

    font-end validation works

--- view/seller/seller_register.php
+++ view/seller/seller_register.php
@@ -8,59 +8,61 @@
     <form class="pt-3" method="post" id="seller_register">
       <div class="form-group">
         <label>Name</label>
-        <input type="text" name="seller_name" required class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Username">
-        <span id="seller_name_error" class="text-danger bold" style="display:none;" >Enter Your Name</span>
+        <input type="text" name="seller_name" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Username">
+        <span id="seller_name_error" class="text-danger bold" style="display:none;">Enter Your Name</span>
       </div>
       <div class="form-group">
         <label>Email</label>
-        <input type="email" name="seller_email" required class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
-        <span id="seller_email_error" class="text-danger bold" style="display:none;" >Enter Correct Email</span>
+        <input type="email" name="seller_email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
+        <span id="seller_email_error" class="text-danger bold" style="display:none;">Enter Correct Email</span>
       </div>
-      <div class="form-group">
-        <select class="form-control form-control-lg" name="country" id="exampleFormControlSelect2">
+      <!-- <div class="form-group">
+        <select class="form-control form-control-lg" name="seller_country" id="exampleFormControlSelect2">
           <option>United States of America</option>
           <option>United Kingdom</option>
           <option>India</option>
           <option>Germany</option>
           <option>Argentina</option>
         </select>
-        <span id="seller_country_error" class="text-danger bold" style="display:none;" >Select County</span>
-      </div>
+        <span id="seller_country_error" class="text-danger bold" style="display:none;">Select County</span>
+      </div> -->
       <div class="form-group">
         <label>Password</label>
-        <input type="password" name="seller_password" required class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-        <span id="seller_password_error" class="text-danger bold" style="display:none;" >Please Enter Valid Password</span>
+        <input type="password" name="seller_password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+        <span id="seller_password_error" class="text-danger bold" style="display:none;">Please Enter Valid Password</span>
       </div>
-      <div class="form-group">
+      <!-- <div class="form-group">
         <label>File</label>
-        <input type="file" name="seller_file" required class="form-control form-control-lg" id="seller_file" placeholder="Password">
-        <span id="seller_file_error" class="text-danger bold" style="display:none;" >Input Imager File</span>
-      </div>
-      <input type="radio" id="html" name="fav_language" value="HTML">
+        <input type="file" name="seller_file" class="form-control form-control-lg" id="seller_file" placeholder="Password">
+        <span id="seller_file_error" class="text-danger bold" style="display:none;">Input Imager File</span>
+      </div> -->
+
+      <!-- <input type="radio" id="html" name="fav_language" value="HTML">
       <label for="html">HTML</label><br>
       <input type="radio" id="css" name="fav_language" value="CSS">
       <label for="css">CSS</label><br>
       <input type="radio" id="javascript" name="fav_language" value="JavaScript">
       <label for="javascript">JavaScript</label>
-      <span id="fav_language_error" class="text-danger bold" style="display:none;" > Please Select Atleast One</span>
+      <span id="fav_language_error" class="text-danger bold" style="display:none;"> Please Select Atleast One</span> -->
+
       <div class="form-group">
         <label>Phone</label>
-        <input class="form-control" id="seller_phone" name="seller_phone" type="tel" placeholder="+123 456 789" required>
-      <span id="seller_phone_error" class="text-danger bold" style="display:none;" >please Provide Phone Number</span>
+        <input class="form-control" id="seller_phone" name="seller_phone" type="tel" placeholder="+123 456 789">
+        <span id="seller_phone_error" class="text-danger bold" style="display:none;">please Provide Phone Number</span>
       </div>
       <div class="form-group">
         <label>Addres</label>
-        <input class="form-control" id="seller_addres" name="seller_addres" type="text" placeholder="123 Street" required>
-      <span id="seller_addres_error" class="text-danger bold" style="display:none;" >please Provide Addres</span>
+        <input class="form-control" id="seller_addres" name="seller_addres" type="text" placeholder="123 Street">
+        <span id="seller_addres_error" class="text-danger bold" style="display:none;">please Provide Addres</span>
       </div>
 
       <div class="mb-4">
         <div class="form-check">
           <label class="form-check-label text-muted">
-            <input type="checkbox" id="seller_terms" name="seller_terms" class="form-check-input" required>
+            <input type="checkbox" id="seller_terms" name="seller_terms" class="form-check-input">
             I agree to all Terms & Conditions
           </label>
-          <span id="seller_terms_error" class="text-danger bold" style="display:none;" >please Agree to terms And Condition</span>     
+          <span id="seller_terms_error" class="text-danger bold" style="display:none;">please Agree to terms And Condition</span>
         </div>
       </div>
       <div class="mt-3">
@@ -80,43 +82,57 @@
       e.preventDefault();
 
       spanErrorArray = validate_data({
-        "#seller_name_error": ($("input[name='seller_name']", '#seller_register')&&$("input[name='seller_name']", '#seller_register').val().trim() !== '') ? true : false,
-        "#seller_addres_error": ($("input[name=seller_addres]", '#seller_register')&&$("input[name=seller_addres]", '#seller_register').val().trim() !== '') ? true : false,
-        "#seller_email_error": ($("input[name='seller_email']", '#seller_register')&&$("input[name='seller_email']", '#seller_register').val() &&
-                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test($("input[type='email'][name='seller_email']", '#seller_register').val())) ? true : false,
-       "#seller_country_error": ($("[name='country]", '#seller_register').prop('selected', true)&&$("[name='country]", '#seller_register').prop('selected', true).is(':selected'))?true:false,
-        "#seller_password_error": ($("input[type='password'][name=seller_password]", '#seller_register') && (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test($("input[type='password'][name=seller_password]", '#seller_register').val()))) ? true : false,
-        "#seller_phone_error": ($("input[type='tel'][name='seller_phone']", '#seller_register') && (/^(\(\d{3}\)|\d{3})[- .]?\d{3}[- .]?\d{4}$/.test($("input[type='tel'][name='seller_phone']", '#seller_register').val().trim()))) ? true : false,
-        "#seller_terms_error": ($("input:checkbox[name='seller_terms']", '#seller_register') && $("input:checkbox[name='seller_terms']", '#seller_register').is(':checked')) ? true : false,
-        "#seller_file_error": ($("input[type='file'][name=seller_file]", '#seller_register') && $("input[type='file'][name=seller_file]", '#seller_register').val() && $("input[type='file'][name=seller_file]", '#seller_register').val().toLowerCase().match(/\.(png|jpg|jpeg|gif|bmp)$/)) ? true : false,
-        "#fav_language_error": ($("input[type='radio'][name='fav_language']:checked") && $("input[type='radio'][name='fav_language']:checked").length > 0) ? true : false,
-        
+        "seller_name_error": ($("input[name='seller_name']", '#seller_register') && $("input[name='seller_name']", '#seller_register').val().trim() !== '') ? true : false,
+        "seller_addres_error": ($("input[name=seller_addres]", '#seller_register') && $("input[name=seller_addres]", '#seller_register').val().trim() !== '') ? true : false,
+        "seller_email_error": ($("input[name='seller_email']", '#seller_register') && $("input[name='seller_email']", '#seller_register').val() &&
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test($("input[type='email'][name='seller_email']", '#seller_register').val())) ? true : false,
+        // "seller_country_error": ($("[name='seller_country']", '#seller_register').prop('selected', true) && $("[name='seller_country']", '#seller_register').prop('selected', true).is(':selected')) ? true : false,
+        "seller_password_error": ($("input[type='password'][name=seller_password]", '#seller_register') && (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test($("input[type='password'][name=seller_password]", '#seller_register').val()))) ? true : false,
+        "seller_phone_error": ($("input[type='tel'][name='seller_phone']", '#seller_register') && (/^(\(\d{3}\)|\d{3})[- .]?\d{3}[- .]?\d{4}$/.test($("input[type='tel'][name='seller_phone']", '#seller_register').val().trim()))) ? true : false,
+        "seller_terms_error": ($("input:checkbox[name='seller_terms']", '#seller_register') && $("input:checkbox[name='seller_terms']", '#seller_register').is(':checked')) ? true : false,
+        // "seller_file_error": ($("input[type='file'][name=seller_file]", '#seller_register') && $("input[type='file'][name=seller_file]", '#seller_register').val() && $("input[type='file'][name=seller_file]", '#seller_register').val().toLowerCase().match(/\.(png|jpg|jpeg|gif|bmp)$/)) ? true : false,
+        // "fav_language_error": ($("input[type='radio'][name='fav_language']:checked") && $("input[type='radio'][name='fav_language']:checked").length > 0) ? true : false,
+
       });
 
-      
+      if (Array.isArray(spanErrorArray) && spanErrorArray.length === 0) {
+        /*let data = new FormData(this);
+        let url = "http://localhost/clones/igotit/seller/create_seller";
+        jQuery.ajax({
+          url: url,
+          data: data,
+          processData: false,
+          contentType: false,
+          type: 'POST',
+          success: function(result) {
+            result = JSON.parse(result);
+            if (result.status == 200) {
+              window.location.href = "http://localhost/clones/igotit/seller/login";
+            } else {
+              console.log(result);
+            };
+          }
+        }); */
+      } else {
+        spanErrorArray.forEach(e => {
+          let input = e.split("_error")[0];
+          console.log($("[name='" + input + "']"));
+          $("[name='" + input + "']").click(function(element) {
+             if ($("#" + e).show()) {
+               $("#" + e).hide();
+             }
+          })
+        });
+      }
 
-      /*let data = new FormData(this);
-      let url = "http://localhost/clones/igotit/seller/create_seller";
-      jQuery.ajax({
-        url: url,
-        data: data,
-        processData: false,
-        contentType: false,
-        type: 'POST',
-        success: function(result) {
-          result = JSON.parse(result);
-          if (result.status == 200) {
-            window.location.href = "http://localhost/clones/igotit/seller/login";
-          } else {
-            console.log(result);
-          };
-        }
-      }); */
+
+
+
+
     });
   });
 </script>
 <script>
-  
   function validate_data(object) {
     let returnArr = [];
     for (const key in object) {
@@ -124,15 +140,14 @@
         const element = object[key];
         if (element == false) {
           returnArr.push(key)
-          $(key).show();
-          
+          $('#'+key).show();
+
+          console.log(key);
         }
-        console.log(key);
       }
     }
     return returnArr;
   }
-
 </script>
 
 
