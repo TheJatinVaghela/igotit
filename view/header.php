b229386421c82e4c841d126c185f2f0d73e56d3f
loljaar <vaghelajatin5@example.com>
2024-03-07 10:36:57 +0530

    yah

--- view/header.php
+++ view/header.php
@@ -13,19 +13,19 @@
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- product page css -->
-    <link href="<?php echo $this->assets;?>lib/slick/slick.css" rel="stylesheet">
-    <link href="<?php echo $this->assets;?>lib/slick/slick-theme.css" rel="stylesheet">
-    <link href="<?php echo $this->assets;?>lib/slick/style.css" rel="stylesheet">
+    <link href="<?php echo $this->assets; ?>lib/slick/slick.css" rel="stylesheet">
+    <link href="<?php echo $this->assets; ?>lib/slick/slick-theme.css" rel="stylesheet">
+    <link href="<?php echo $this->assets; ?>lib/slick/style.css" rel="stylesheet">
     <!-- product page css-end -->
 
     <!-- Libraries Stylesheet -->
-    
+
     <link href="../public\assets\lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
@@ -33,14 +33,14 @@
 
     <!-- jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    
+
 </head>
 
 <body>
     <!-- Topbar Start -->
     <div class="container-fluid">
         <div class="row bg-secondary py-2 px-xl-5">
-           
+
             <div class="col-lg-6 text-center text-lg-right">
                 <div class="d-inline-flex align-items-center">
                     <a class="text-dark px-2" href="">
@@ -91,8 +91,8 @@
             </div>
         </div>
     </div>
-    
-           
+
+
     <nav class="container navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
         <a href="http://localhost/clones/igotit/public/home" class="text-decoration-none d-block d-lg-none">
             <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
@@ -102,51 +102,51 @@
         </button>
         <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
             <div class="navbar-nav mr-auto py-0">
-                <a href="http://localhost/clones/igotit/public/home" class="nav-item nav-link<?php if($this->page_name == '/home'){echo " active";}?>">Home</a>
-                <a href="http://localhost/clones/igotit/public/shop" class="nav-item nav-link<?php if($this->page_name == '/shop'){echo " active";}?>">Shop</a>
-                <a href="http://localhost/clones/igotit/public/detail" class="nav-item nav-link<?php if($this->page_name == '/detail'){echo " active";}?>">Shop Detail</a>
-                <a href="http://localhost/clones/igotit/public/cart" class="nav-item nav-link<?php if($this->page_name == '/cart'){echo " active";}?>">Shopping Cart</a>
-                <a href="http://localhost/clones/igotit/public/checkout" class="nav-item nav-link<?php if($this->page_name == '/checkout'){echo " active";}?>">Checkout</a>
-                <a href="http://localhost/clones/igotit/public/contact" class="nav-item nav-link<?php if($this->page_name == '/contact'){echo " active";}?>">Contact</a>
-                <nav class="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0" id="navbar-vertical">
-                Categories    
-                <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
-                <?php foreach($this->data as $key => $value){?>
-                    <div class="nav-item dropdown">
-                        <a class="nav-link"  <?php if(isset($value[0]) && !empty($value[0])){echo "data-toggle='dropdown'"; };?> ><?php echo $key; ?> <?php if(isset($value[0]) && !empty($value[0])){echo '<i class="fa fa-angle-down float-right mt-1"></i>' ;}?></a>
-                        <div class="dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0">
-                            <?php foreach($value as $key1 => $value1){ 
-                                    if(!empty($value1) && $value1 != null){  ?>
-                                        <a href="http://localhost/clones/igotit/public/shop?category=<?php echo $key;?>&subcategory=<?php echo $value1;?>" class="dropdown-item"><?php echo $value1;?></a>
-                                <?php  };
-                            } ?>
-                        </div>
-                    </div>
-                <?php };?>
-                </div>
-            </nav>
+                <a href="http://localhost/clones/igotit/public/home" class="nav-item nav-link<?php if ($this->page_name == '/home') {
+                                                                                                    echo " active";
+                                                                                                } ?>">Home</a>
+                <a href="http://localhost/clones/igotit/public/shop" class="nav-item nav-link<?php if ($this->page_name == '/shop') {
+                                                                                                    echo " active";
+                                                                                                } ?>">Shop</a>
+                <a href="http://localhost/clones/igotit/public/detail" class="nav-item nav-link<?php if ($this->page_name == '/detail') {
+                                                                                                    echo " active";
+                                                                                                } ?>">Shop Detail</a>
+                <a href="http://localhost/clones/igotit/public/cart" class="nav-item nav-link<?php if ($this->page_name == '/cart') {
+                                                                                                    echo " active";
+                                                                                                } ?>">Shopping Cart</a>
+                <a href="http://localhost/clones/igotit/public/checkout" class="nav-item nav-link<?php if ($this->page_name == '/checkout') {
+                                                                                                        echo " active";
+                                                                                                    } ?>">Checkout</a>
+                <a href="http://localhost/clones/igotit/public/contact" class="nav-item nav-link<?php if ($this->page_name == '/contact') {
+                                                                                                    echo " active";
+                                                                                                } ?>">Contact</a>
+
             </div>
-            <?php 
-                if(isset($_COOKIE['customer_id'])){ ?>
+            <?php
+            if (isset($_COOKIE['customer_id'])) { ?>
                 <div class="navbar-nav  py-0">
                     <a class="nav-item nav-link" onclick="removeUser()">LogOut</a>
                 </div>
             <?php } else { ?>
                 <div class="navbar-nav  py-0">
-                    <a href="http://localhost/clones/igotit/public/login" class="nav-item nav-link<?php if($this->page_name == '/login'){echo " active";}?>">Login</a>
-                    <a href="http://localhost/clones/igotit/public/register" class="nav-item nav-link<?php if($this->page_name == '/register'){echo " active";}?>">Register</a>
+                    <a href="http://localhost/clones/igotit/public/login" class="nav-item nav-link<?php if ($this->page_name == '/login') {
+                                                                                                        echo " active";
+                                                                                                    } ?>">Login</a>
+                    <a href="http://localhost/clones/igotit/public/register" class="nav-item nav-link<?php if ($this->page_name == '/register') {
+                                                                                                            echo " active";
+                                                                                                        } ?>">Register</a>
                 </div>
-            <?php };?>
+            <?php }; ?>
         </div>
     </nav>
-            
-    
+
+
     <!-- Topbar End -->
 
 
-   <script>
-    function removeUser(){
-        document.cookie = "customer_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        window.location.href="http://localhost/clones/igotit/public/login";
-    }
-   </script>
+    <script>
+        function removeUser() {
+            document.cookie = "customer_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            window.location.href = "http://localhost/clones/igotit/public/login";
+        }
+    </script>
